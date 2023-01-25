ae1e27436057cea7a2b52d383d84808df9822026
DenisCuenca <dcbuele@gmail.com>
2023-01-24 22:30:36 -0500

    🚚 routes added

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useUserContext } from "../../context/userContext";
 import ReportsForm from "../reports/ReportsForm";
 import { redirect, useNavigate } from "react-router-dom";
-
+import UploadReport from "../reports/UploadReport";
 const Dashboard = () => {
   const { user, logoutUser } = useUserContext();
   // const navigate = useNavigate()
@@ -12,14 +12,21 @@ const Dashboard = () => {
   
 
   return (
-    <div>
+    <>
+      <div className="userInfo">
+
       <h2>this is the dashboard</h2>
       <p>Name: {user.displayName}</p>
       <p>Email: {user.email}</p>
       <a href = "/signin" onClick={logoutUser}>logout</a>
 
-      <ReportsForm />
-    </div>
+      </div>
+      
+      <UploadReport/>
+
+
+    
+    </>
   );
 };
 
